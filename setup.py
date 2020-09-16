06297b8af2fcaf66d8571aaffa947ff242a1e577
fiacovelli <f.iacovelli@4sigma.it>
2020-09-16 10:59:24 +0200

    New release on pip

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django_video_embedding',
-    version='0.1.1',
+    version='1.0',
     description='Support for video upload in Django models',
     long_description='Support for video upload in Django models',
     author='4Sigma',
